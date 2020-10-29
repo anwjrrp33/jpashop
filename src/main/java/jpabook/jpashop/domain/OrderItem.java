0a572b282ec99eb75ef3a273bd86f58ce88a122f
anwjrrp33 <anwjrrp33@gmail.com>
2020-10-29 10:41:24 +0900

    update

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,5 +16,16 @@ import lombok.Setter;
 public class OrderItem {
 
     @Id @GeneratedValue
+    @Column(name = "order_item_id")
     private Long id;
+
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; // 주문 가격
+
+    private int count; // 주문 수량
 }
